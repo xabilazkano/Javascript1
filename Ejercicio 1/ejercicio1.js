60ee1ba982188d0b805ee4e7d38f3f34343a86d1
agomezdo18dw <agomezdo18dw@ikzubirimanteo.com>
2019-09-25 14:24:44 +0200

    Ariketak bukatuta

---
+++ Ejercicio 1/ejercicio1.js
@@ -0,0 +1,6 @@
+let nombre = "xabi";
+let apellido = "lazkano";
+let edad = 22;
+let salario= 1500;
+
+alert("Mi nombre es "+nombre+" y mi apellido es "+apellido+"\n Mi edad es "+edad+" y mi salario es "+salario);
